39678d55b740895437a162729943a4445b93d59a
Arezoo <138774289+ZahraAbbakhsh99@users.noreply.github.com>
2026-07-16 11:50:13 +0330

    feat: add rejectionReason field to all ad models

---
+++ prisma/migrations/20260715105208_add_rejection_reason_to_ads/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE "DigitalAd" ADD COLUMN     "rejectionReason" TEXT;
+
+-- AlterTable
+ALTER TABLE "EmployerAd" ADD COLUMN     "rejectionReason" TEXT;
+
+-- AlterTable
+ALTER TABLE "JobSeekerAd" ADD COLUMN     "rejectionReason" TEXT;
+
+-- AlterTable
+ALTER TABLE "SellerAd" ADD COLUMN     "rejectionReason" TEXT;
